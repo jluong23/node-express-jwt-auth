ddcf7d1bfdb585db18702cf096d1d6ebc6657136
James Luong <jamesluong@hotmail.co.uk>
2022-08-24 23:11:01 +0100

    Implemented login with feedback for incorrect credentials

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,37 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const handleErrors = (err) => {
+const handleSignUpErrors = (err) => {
     let errors = {
         "email" : "",
         "password": "",
     }
 
-    // duplicate email error
+    // Duplicate / existing email error
     if(err.code == 11000){
         errors.email = "That email has already been taken!"
     }
-    // validation errors 
+    // validation errors upon sign up
+    // err.errors.properties contains error messages for each path.
+    // assign onto errors variable for returning to client
     if(err.message.includes('User validation failed:')){
         Object.values(err.errors).forEach(({properties}) => {
             errors[properties.path] = properties.message;
         });
     }
+    
+    return errors;
+}
+
+const handleLoginErrors = (err) => {
+    let errors = {
+        "email" : "",
+        "password": "",
+    }
+
+    if(err.message === "Incorrect email and password combination, please try again."){
+        errors.password = err.message; //assign onto password error field only, don't want to show twice on client side
+    }
+
     return errors;
 }
 const maxTokenAgeSeconds = 24*60*60 //JWT max token age in seconds
@@ -47,17 +63,36 @@ const sign_up_post = async (req, res) => {
             sameSite: "strict"
             
         })
+        console.log(`User with email ${email} has been created`);
         res.status(201).json({user: user._id});
 
     }catch(err){
         //could not register, send feedback to client after handling errors
-        const errors = handleErrors(err);
+        const errors = handleSignUpErrors(err);
         res.status(400).json({errors});
     }
 }
 
 const login_post = async (req, res) => {
-    res.send('user login');
+    const {email, password} = req.body;
+    try{
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        // login successful
+        // set a jwt cookie on response.  
+        res.cookie('jwt', token, {
+            httpOnly: true, //Only transmitable by http
+            maxAge: maxTokenAgeSeconds * 1000, // Max age is in ms, so *1000
+            sameSite: "strict"
+            
+        })
+        console.log(`User with email ${email} has logged in`);
+        res.status(201).json({user: user._id});
+    }catch(err){
+        // login unsuccessful, send feedback to client after handling errors
+        const errors = handleLoginErrors(err);
+        res.status(400).json({errors});
+    }
 }
 
 module.exports = {sign_up_get, login_get, sign_up_post, login_post}
--- models/User.js
+++ models/User.js
@@ -18,18 +18,33 @@ const userSchema = mongoose.Schema({
 })
 
 // mongoose hooks
-// runs after doc is saved to db
-userSchema.post('save', (doc, next) => {
-    console.log(`User with email ${doc.email} has been saved.`);
-    next();
-})
-
 // runs before doc saved to db
 userSchema.pre('save', async function (next) {
     let salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
 })
+
+// static methods
+userSchema.statics.login = async function(email, password){
+    const user = await User.findOne({email});
+    if(user){
+        // compare given password with hashed user password
+        const passwordCorrect = await bcrypt.compare(password, user.password);
+        if(passwordCorrect){
+            // login the user
+            return user;
+        }else{
+            // password was incorrect
+            throw Error("Incorrect email and password combination, please try again.")
+        }
+    }else{
+        // user does not exist
+        throw Error("Incorrect email and password combination, please try again.")
+    }
+
+
+}
 
 
 const User = mongoose.model('User', userSchema);
